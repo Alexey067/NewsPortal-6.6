7ab9e3d257577ab9516259e91d32befab200ca6c
Alexey067 <115927697+Alexey067@users.noreply.github.com>
2023-01-18 22:23:07 +0300

    NewsPortal 6.6
    6.6

---
+++ NewsPortal/news/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-date_post'
+    template_name = 'flatpages/news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'flatpages/new.html'
+    context_object_name = 'new'
+
+# Create your views here.
